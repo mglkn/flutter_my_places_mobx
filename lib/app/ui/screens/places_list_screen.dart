9f3024eadb65b97fab0e8ae1ce16f644a314f7f5
Vladimir <iammigalkin@yandex.com>
2020-03-02 20:27:07 +0300

    show draft list places;

--- lib/app/ui/screens/places_list_screen.dart
+++ lib/app/ui/screens/places_list_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import '../../data/db_repository.dart';
+import '../../data/db.dart';
 
 class PlacesListScreen extends StatelessWidget {
   static String routeName = '/';
@@ -6,9 +10,49 @@ class PlacesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('places list'),
-      ),
+      appBar: _appBarBuild(),
+      body: PlacesList(),
     );
+  }
+
+  _appBarBuild() {
+    return AppBar(
+      title: Text(
+        'My Places',
+        style: TextStyle(color: Colors.grey[700]),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.grey[100],
+      elevation: 0.0,
+    );
+  }
+}
+
+class PlacesList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final repo = Modular.get<DbDataRepository>();
+
+    return StreamBuilder<List<Place>>(
+      initialData: [],
+      stream: repo.watchPlaces(),
+      builder: (_, AsyncSnapshot<List<Place>> places) {
+        return ListView.builder(
+          itemCount: places.data.length,
+          itemBuilder: (_, int index) => PlaceTile(places.data[index]),
+        );
+      },
+    );
+  }
+}
+
+class PlaceTile extends StatelessWidget {
+  final Place place;
+
+  PlaceTile(this.place);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(place.name);
   }
 }
