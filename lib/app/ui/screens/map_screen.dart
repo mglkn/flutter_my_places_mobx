5765e6d17f2f53a3f7bbd626f9286f820dea18c9
Vladimir <iammigalkin@yandex.com>
2020-03-03 10:38:45 +0300

    add map_screen appbar;

--- lib/app/ui/screens/map_screen.dart
+++ lib/app/ui/screens/map_screen.dart
@@ -6,9 +6,23 @@ class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: _appBarBuild(),
       body: Center(
         child: Text('map screen'),
       ),
+    );
+  }
+
+  _appBarBuild() {
+    return AppBar(
+      title: Text(
+        'Map',
+        style: TextStyle(color: Colors.grey[700]),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.grey[100],
+      iconTheme: IconThemeData(color: Colors.grey[700]),
+      elevation: 0.0,
     );
   }
 }
