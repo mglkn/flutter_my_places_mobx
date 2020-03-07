b75f4a5086b6026a85da15d295719e13f3de8593
Vladimir <iammigalkin@yandex.com>
2020-03-07 11:53:49 +0300

    add dismiss place;

--- lib/app/ui/screens/places_list_screen.dart
+++ lib/app/ui/screens/places_list_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
 import 'dart:convert';
+import 'package:async/async.dart' show StreamGroup;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:my_places/app/ui/screens/screens.dart';
 
 import '../../data/db_repository.dart';
 import '../../data/db.dart';
@@ -83,23 +86,123 @@ class AppBarLeading extends StatelessWidget {
 }
 
 class PlacesList extends StatelessWidget {
-  EPlaceOrder order;
+  final EPlaceOrder order;
+  final repo = Modular.get<DbDataRepository>();
 
   PlacesList(this.order);
 
   @override
   Widget build(BuildContext context) {
-    final repo = Modular.get<DbDataRepository>();
-
     return StreamBuilder<List<Place>>(
       initialData: [],
       stream: repo.watchPlaces(order: order),
       builder: (_, AsyncSnapshot<List<Place>> places) {
         return ListView.builder(
           itemCount: places.data.length,
-          itemBuilder: (_, int index) => PlaceTile(places.data[index]),
+          itemBuilder: (_, int index) => DismissibleWrapper(
+            key: ObjectKey(places.data[index]),
+            child: PlaceTile(places.data[index]),
+            place: places.data[index],
+          ),
         );
       },
+    );
+  }
+}
+
+class DismissibleWrapper extends StatefulWidget {
+  final Widget child;
+  final Place place;
+  final Key key;
+  final repo = Modular.get<DbDataRepository>();
+  final deleteDelay = const Duration(milliseconds: 3000);
+  final showSnackBarDelay = const Duration(milliseconds: 2700);
+
+  DismissibleWrapper({this.key, this.child, this.place});
+
+  @override
+  _DismissibleWrapperState createState() => _DismissibleWrapperState();
+}
+
+class _DismissibleWrapperState extends State<DismissibleWrapper> {
+  StreamController streamControllerCancel;
+  StreamController streamControllerDone;
+  StreamSubscription resultStream;
+
+  void _closeStreams() {
+    resultStream?.cancel();
+    streamControllerCancel?.close();
+    streamControllerDone?.close();
+  }
+
+  // Create two streams (done and cancel), merge them and
+  // evaluate merged stream as Future.
+  // `Done` stream with delay and pass `true` value.
+  // `Cancel` stream pass `false` and can called
+  // through `streamControllerCancel.sink.add(null)`
+  Future<bool> _onDismissed(_) async {
+    _closeStreams();
+
+    streamControllerCancel = StreamController<bool>();
+    streamControllerDone = StreamController<bool>();
+
+    final streamCancel = streamControllerCancel.stream.map<bool>((_) => false);
+    final streamDone = streamControllerDone.stream.asyncMap<bool>((_) async {
+      await Future.delayed(widget.deleteDelay);
+      return true;
+    });
+
+    bool resultDesision;
+    resultStream = StreamGroup.merge<bool>([streamDone, streamCancel])
+        .take(1)
+        .asBroadcastStream()
+        .listen(
+      (bool desision) {
+        resultDesision = desision;
+      },
+    );
+
+    _showCancelSnackBar();
+
+    streamControllerDone.sink.add(null);
+
+    await resultStream.asFuture();
+
+    if (resultDesision) {
+      await widget.repo.removePlace(widget.place);
+    }
+
+    return resultDesision;
+  }
+
+  _showCancelSnackBar() {
+    final snackbar = SnackBar(
+      duration: widget.showSnackBarDelay,
+      content: Text('Are you shure?'),
+      action: SnackBarAction(
+        label: 'Undo',
+        onPressed: () {
+          streamControllerCancel.sink.add(null);
+        },
+      ),
+    );
+
+    Scaffold.of(context).showSnackBar(snackbar);
+  }
+
+  @override
+  void dispose() {
+    _closeStreams();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      direction: DismissDirection.endToStart,
+      confirmDismiss: _onDismissed,
+      key: widget.key,
+      child: widget.child,
     );
   }
 }
@@ -121,7 +224,7 @@ class PlaceTile extends StatelessWidget {
           color: Colors.grey[200],
           border: Border(
             top: BorderSide(
-              color: Colors.grey[400],
+              color: Colors.grey[300],
               width: 2.0,
             ),
           ),
