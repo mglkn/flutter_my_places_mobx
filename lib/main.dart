a14d58a77969e2a3f741a5f879aa5fe516368b1a
vldmr <thewumin@gmail.com>
2020-03-16 17:43:45 +0300

    add fix orientation;

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+// import 'package:flutter/rendering.dart';
 
 import 'app/app_module.dart';
 
-void main() => runApp(ModularApp(module: AppModule()));
+void main() {
+  // debugPaintPointersEnabled = debugPaintBaselinesEnabled =
+  //     debugPaintLayerBordersEnabled = debugRepaintRainbowEnabled = true;
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (_) => runApp(ModularApp(module: AppModule())),
+  );
+}
